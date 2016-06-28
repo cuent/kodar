87b9bdd40f5218db8e0574f3ca61ee0aa1d21747
cuent <c.uent@hotmail.com>
2016-06-27 22:51:13 -0500

    Update paths for named clusters
    Signed-off-by: Xavier Sumba <xavier.sumba93@ucuenca.ec>

--- Job/src/main/java/edu/uc/mahout/base/topicmodel/Tagger.java
+++ Job/src/main/java/edu/uc/mahout/base/topicmodel/Tagger.java
@@ -5,8 +5,8 @@
  */
 package edu.uc.mahout.base.topicmodel;
 
-import edu.ucuenca.kodar.clusters.MahoutController;
-import edu.ucuenca.kodar.utils.KODAReader;
+import edu.ucuenca.kodar.clusters.Controller;
+import edu.ucuenca.kodar.clusters.ControllerImpl;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -23,18 +23,15 @@ public class Tagger {
     private static final String BASE_PATH = System.getProperty("user.dir") + "/";
 
     public String tag(String inputPath) throws IOException, Exception {
-        KODAReader mr = KODAReader.getReadSequenceFile();
-        MahoutController base = new MahoutController();
-
         //Hadoop Configuration
         Configuration conf = new Configuration();
 
-        //base.seqDirectoryToText(conf, "mahout-base/original/labels.csv", "mahout-base/topmodel/output");
-        //mr.readTT(inputPath);
+        Controller base = new ControllerImpl(conf);
+
         //Generate Sparse Vectors
         String[] seq2sparse = new String[]{
             "-i", inputPath,
-            "-o", BASE_PATH + "mahout-base/topmodel/sparse",
+            "-o", BASE_PATH + "target/kodar_home/topmodel/sparse",
             "--maxDFPercent", "60",
             "-n", "2",
             "-ng", "2",
@@ -43,40 +40,34 @@ public class Tagger {
             "-seq",
             "-ow"
         };
-        base.seq2Sparse(conf, seq2sparse);
-//        mr.readIL("mahout-base/topmodel/sparse/df-count/part-r-00000");
-//        mr.readTI("mahout-base/topmodel/sparse/dictionary.file-0");
-//        mr.readIL("mahout-base/topmodel/sparse/frequency.file-0");
-//        mr.readTV("mahout-base/topmodel/sparse/tf-vectors/part-r-00000");
-//        mr.readTS("mahout-base/topmodel/sparse/tokenized-documents/part-m-00000");
-        //mr.readTL("mahout-base/topmodel/sparse/wordcount/part-r-00000");
+        base.seq2Sparse(seq2sparse);
 
         //Convert to <SequenceFile,SequenceFile>
         String[] rowIdArgs = new String[]{
-            "-i", BASE_PATH + "mahout-base/topmodel/sparse/tf-vectors",
-            "-o", BASE_PATH + "mahout-base/topmodel/convert"
+            "-i", BASE_PATH + "target/kodar_home/topmodel/sparse/tf-vectors",
+            "-o", BASE_PATH + "target/kodar_home/topmodel/convert"
         };
-        base.rowId(conf, rowIdArgs);
+        base.rowId(rowIdArgs);
 
-        Path path_cvb = new Path(BASE_PATH + "mahout-base/topmodel/cvb");
+        Path path_cvb = new Path(BASE_PATH + "target/kodar_home/topmodel/cvb");
         HadoopUtil.delete(conf, path_cvb);
         //Run collapse variational bayes algorithm
         String[] cvb = new String[]{
-            "-i", BASE_PATH + "mahout-base/topmodel/convert/matrix",
-            "-dict", BASE_PATH + "mahout-base/topmodel/sparse/dictionary.file-*",
-            "-o", BASE_PATH + "mahout-base/topmodel/cvb/topic-term",
-            "-dt", BASE_PATH + "mahout-base/topmodel/cvb/doc-topic/topic-model-cvb",
-            "-mt", BASE_PATH + "mahout-base/topmodel/cvb",
+            "-i", BASE_PATH + "target/kodar_home/topmodel/convert/matrix",
+            "-dict", BASE_PATH + "target/kodar_home/topmodel/sparse/dictionary.file-*",
+            "-o", BASE_PATH + "target/kodar_home/topmodel/cvb/topic-term",
+            "-dt", BASE_PATH + "target/kodar_home/topmodel/cvb/doc-topic/topic-model-cvb",
+            "-mt", BASE_PATH + "target/kodar_home/topmodel/cvb",
             "-k", "1",
             "-x", "20",
             "-ow"
         };
-        base.collapsedVariationalBayes(conf, cvb);
+        base.collapsedVariationalBayes(cvb);
 
         String[] vectorDump = new String[]{
-            "-i", BASE_PATH + "mahout-base/topmodel/cvb/topic-term/",
-            "-o", BASE_PATH + "mahout-base/topmodel/vectorDump",
-            "-d", BASE_PATH + "mahout-base/topmodel/sparse/dictionary.file-0",
+            "-i", BASE_PATH + "target/kodar_home/topmodel/cvb/topic-term/",
+            "-o", BASE_PATH + "target/kodar_home/topmodel/vectorDump",
+            "-d", BASE_PATH + "target/kodar_home/topmodel/sparse/dictionary.file-0",
             "-vs", "1",
             "-dt", "sequencefile",
             "-p", "true",
@@ -85,7 +76,7 @@ public class Tagger {
         base.vectorDump(vectorDump);
 
         //Read label from text file
-        BufferedReader br = new BufferedReader(new FileReader(BASE_PATH + "mahout-base/topmodel/vectorDump"));
+        BufferedReader br = new BufferedReader(new FileReader(BASE_PATH + "target/kodar_home/topmodel/vectorDump"));
         String line = br.readLine();
         if (line != null) {
             line = line.substring(line.indexOf("{") + 1, line.indexOf(":"));
